85c40df097d5dabfe6e6107f156b845bd0bed30b
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-05 14:50:07 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main: is thr main function
+ *printf formats and prints data
+ *Return: returns value 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
