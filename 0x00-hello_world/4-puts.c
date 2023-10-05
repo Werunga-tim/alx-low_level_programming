1fd07ea9ce9568b49edeec046ed483f30d210fe4
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-05 14:29:45 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return:  end the program with value 0
+ * put: appends a new line at the end of the output
+ *
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
