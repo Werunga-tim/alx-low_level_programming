469871875535f5350dbb5ed38a5d513b0445abde
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-11 16:49:31 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase
+ * Retunrs: nothing
+*/
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		_putchar(x);
+	_putchar('\n');
+}
