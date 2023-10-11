853f2b6bad58fb2dd79aa5cb24f320d29b883c65
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-11 16:32:59 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - main function of the program that prints _putchar
+ * Return: the value 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
