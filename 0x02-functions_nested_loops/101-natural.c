f0094a078f92eea2dff0f7322695bc0ed951721e
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-12 11:50:40 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Main function of the program
+ *
+ * Description: Computes and prints the sum of all multiples of 3 or 5 below 1024.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main() {
+    int sum = 0;
+    int cap = 1024;
+
+    for (int i = 0; i < cap; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+
+    printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
+
+    return 0;
+}
