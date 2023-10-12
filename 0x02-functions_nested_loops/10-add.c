84de9e097acd370d6e560f69fd3324dffc383271
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-11 19:56:11 -0700

    task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @x: integer one
+ * @y: integer two
+ *
+ * Return:sum.
+ *
+ */
+
+
+int add(int x, int y)
+{
+	int sum;
+
+
+	sum = x + y;
+	return (sum);
+}
+
