b7d497850783b332c2a6fd02be0dec2c60109fde
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-11 18:40:51 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer -function that prints every minute of the day of Jack Bauer
+ * Return: nothing
+*/
+
+void jack_bauer(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 24; y++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+		}
+	}
+}
