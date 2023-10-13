a1575716a450c224932a878cad4510b373812db3
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-13 11:01:28 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul -  a function that multiplies two integers
+ *@a:parameter to be cheecked
+ *@b:parameter to be checked
+ *Return: a*b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
