85a66dc599f736488b079ded71496c6463deda50
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-13 13:48:18 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_diagonal - draws a diagonal line on the terminal.
+ *@n: parameter being checked
+ *
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+		int y;
+
+		for (x = 1; x <= n; x++)
+		{
+			for (y = 0; y <= x; y++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
