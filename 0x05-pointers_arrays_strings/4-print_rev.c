ccfb32ec9530ec10e76c56369e01d55a40c1dd9a
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-17 19:23:16 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: string to be printed
+ * Retunr: void
+ */
+
+void print_rev(char *s)
+{
+	int count = -1;
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+		count++;
+	for (x = count -1 ; x >= 0 ; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
