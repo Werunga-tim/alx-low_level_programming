35020a75ee11cccd973ab4568c43d5e77bd0d04c
Timothy Werunga <timothy.werunga@outlook.com>
2023-10-19 18:11:17 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes to copy
+ * Return: dest
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0 ; x < n && src[x] != '\0' ; x++)
+		dest[x] = src[x];
+	while (x < n)
+	{
+		dest[x] = '\0';
+		x++;
+	}
+	return (dest);
+}
